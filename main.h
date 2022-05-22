c8e9ecc99eaaac440360570e1f549c9021dfa11b
chindapikin <favchi22@gmail.com>
2022-05-22 02:51:48 -0700

    Updating main.h

--- main.h
+++ main.h
@@ -4,6 +4,15 @@
 
 int _putchar(char c);
 int _printf(const char *format, ...);
+int _print_int(va_list args);
+int _print_char(va_list args);
+int _print_str(va_list args);
+int _print_bin(va_list args);
+int _print_dec(va_list args);
+int _print_perc(va_list args);
+int (*_select_func(const char c))(va_list);
+int _print_octal(va_list args);
+int _print_unint(va_list args);
 
 
 #endif
